49143ca1dd0ddb164389b553fe20810614ef5714
Ilya <sok203333@gmail.com>
2025-03-01 16:47:13 +0300

    add class Ping and rework functionality

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
+from typing import Literal
+
 from fastapi import FastAPI
 
-from src.monitoring.services.ping import ping
+from src.monitoring.services.ping import Ping
 
 app = FastAPI()
 
 
 @app.get("/ping/")
-async def ping_url(
-    url: str,
-    params: dict | None = None,
-):
-    return await ping(url=url, params=params or {})
+async def ping_url(url: str,
+                   method: Literal['get', 'post', 'put', 'delete'],
+                   params: dict | None = None,
+                   ) -> dict:
+    ping = Ping(url=url, method=method, params=params)
+    result = await ping.process()
+    return dict(result=result)
--- src/monitoring/services/ping.py
+++ src/monitoring/services/ping.py
@@ -1,28 +1,51 @@
-import aiohttp
-import ssl
+from typing import Literal
+
+from httpx import AsyncClient
+# import Exceptions
+from httpx import RequestError, HTTPStatusError
 
 
-def safe_url(url: str) -> str:
-    if not url.startswith(('http://', 'https://')):
-        return f'https://{url}'
-    return url
+class Ping:
+    def __init__(self,
+                 url: str,
+                 method: Literal['get', 'post', 'put', 'delete'],
+                 params: dict = None):
+        self.url = self.safe_url(url=url)
+        self.method = method
+        self.params = params if params else {}
 
+    async def process(self):
+        try:
+            async with AsyncClient() as client:
+                return await self.request(client)
+        except Exception as e:
+            return {'error': str(e)}
 
-async def ping(url: str, params: dict | None = None):
-    try:
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-        }
-        ssl_context = ssl.create_default_context()
-        ssl_context.check_hostname = False
-        ssl_context.verify_mode = ssl.CERT_NONE
+    async def request(self,
+                      client: AsyncClient):
+        http_method = getattr(client, self.method)
+        try:
+            response = await http_method(url=self.url, params=self.params)
+            response.raise_for_status()
+            result = dict(
+                status_code=response.status_code,
+                content=response.content
+            )
+        except RequestError as exc:
+            result = dict(
+                error=f"An error occurred while requesting {exc.request.url!r}.",
+            )
+        except HTTPStatusError as exc:
+            result = dict(
+                status_code=exc.response.status_code,
+                error=f"Error response {exc.response.status_code} while requesting {exc.request.url!r}.",
+                reason=exc.response.reason_phrase,
+            )
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(safe_url(url), params=params) as response:
-                return {
-                    'url': url,
-                    'status': response.status,
-                    'ok': response.ok,
-                }
-    except Exception as e:
-        return {'error': str(e)}
+        return result
+
+    @staticmethod
+    def safe_url(url: str) -> str:
+        if not url.startswith(('http://', 'https://')):
+            return f'https://{url}'
+        return url
