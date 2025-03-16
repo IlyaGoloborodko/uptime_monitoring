e76f86fa71f465353d1d8f9abbe2719415794cb5
Ilya <sok203333@gmail.com>
2025-03-16 17:25:13 +0300

    add docker compose. Add pycharm config for debug with docker container

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from backend.app.api.main import api_router
@@ -5,3 +6,6 @@ from backend.app.api.main import api_router
 app = FastAPI()
 
 app.include_router(api_router)
+
+if __name__ == '__main__':
+    uvicorn.run("backend.app.main:app", host='0.0.0.0', port=80, reload=True)
